88199a1d0717d6f5fa6d788bb037032dcd940713
niole <niolenelson@gmail.com>
2017-02-19 16:14:20 -0800

    can add words and find matches for words

--- src/trie.scala
+++ src/trie.scala
@@ -1,33 +1,88 @@
 //every level is a sorted list with single character prefixes
 object Main extends App {
  val word = "abc"
- val T = PrefixTrie(None, word.toList)
- println(T)
+ val w2 = "abc"
+ val T = PrefixTrie(None, word.toList, word)
+ T.addWord(w2.toList, w2)
+ val matches = T.findMatches(word.toList, List[String]())
+ val ms = T.findMatches(w2.toList, List[String]())
 }
 
 
-case class PrefixTrie(chr: Option[Char], word: List[Char], var children: List[PrefixTrie] = List[PrefixTrie]()) {
- children = addWord(word)
+case class PrefixTrie(chr: Option[Char], letters: List[Char], private val word: String, var children: List[PrefixTrie] = List[PrefixTrie]()) {
+ children = addWord(letters, word)
+ var fullWord = ""
+ setWord(word)
 
- private[this] def addChild(word: List[Char]): List[PrefixTrie] = word match {
-  case head::tail =>
-   val newNode = PrefixTrie(Option(head), tail)
-   newNode :: children
+ def getWord: String = fullWord
+ def setWord(newWord: String): Unit = {
+  if (letters.isEmpty) {
+   fullWord = newWord
+  }
  }
 
- def addWord(word: List[Char]): List[PrefixTrie] = word match {
-  case Nil => List[PrefixTrie]()
+ private[this] def addChild(letters: List[Char], word: String): List[PrefixTrie] = letters match {
+  case head::tail =>
+   val newNode = PrefixTrie(Option(head), tail, word)
+   children = newNode :: children
+   children
+ }
+
+ def accumulateWord(found: List[String]): List[String] = {
+  val foundWord = getWord
+  var foundWords = found
+  if (foundWord != "") {
+   foundWords = foundWord::found
+  }
+  foundWords
+ }
+
+ def findMatches(letters: List[Char] = List[Char](), found: List[String] = List[String]()): List[String] = letters match {
+  case Nil =>
+   //get all, if anymore exist
+   val updatedFound = accumulateWord(found)
+   getAllPossibilities(updatedFound)
+  case head::tail =>
+     //keep going along matching trail
+     //if stop matching, return found
+     val updatedFound = accumulateWord(found)
+     val matchingChild = getMatchingChild(head)
+     matchingChild.map(child => child.findMatches(tail, updatedFound)).getOrElse(updatedFound)
+ }
+
+ def getAllPossibilities(found: List[String]): List[String] = {
+  if (children.nonEmpty) {
+   children.flatMap(child => child.findMatches(List[Char](), found))
+  }
+  found
+ }
+
+ def getMatchingChild(chr: Char): Option[PrefixTrie] = {
+  for {
+   child <- children
+  } {
+   if (child.chr.getOrElse("") == chr) {
+    return Some(child)
+   }
+  }
+  None
+ }
+
+ def addWord(letters: List[Char], word: String): List[PrefixTrie] = letters match {
+  case Nil =>
+   setWord(word)
+   List[PrefixTrie]()
   case head::tail =>
    for {
     child <- children
    } {
     if (child.chr.getOrElse("") == head) {
-     return child.addWord(tail)
+     return child.addWord(tail, word)
     }
    }
 
    //not in children
-   addChild(word)
+   addChild(letters, word)
  }
 
 }
